48f73225dd7f7cab046bc13fdf67202e178567a2
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2024-08-29 17:49:12 +0400

    First lesson

---
+++ 1_28.08.2024/homework_09#1.py
@@ -0,0 +1,82 @@
+"""
+- Printing -
+A. Write a simple program that prints "Python is the beast!"
+B. Write a simple program that prints "I love coding."
+C. Write a simple program that prints "I am going to master Python soon"
+D. Write a simple program that prints "I am learning Python, hooray!!"
+E. Write a simple program that prints "Python is the most powerful programming language!"
+
+- Variables -
+A. Create a variable named "fruit" and assign any fruit-related string to it. Print that
+variable.
+B. Create a variable named "sentence" and assign any string to it. Print that
+variable.
+C. Create a variable named "name" and assign any name-related string to it. Print that
+variable.
+D. Create a variable named "surname" and assign any name-related string to it. Print that
+variable.
+E. Create a variable named "full_name" and assign any name-related string to it. Print that
+variable.
+F. Create a variable named "colour" and assign any colour-related string to it. Print that
+variable.
+G. Create a variable named "gender" and assign any gender-related string to it. Print that
+variable.
+H. Create a variable named "brand" and assign any brand-related string to it. Print that
+variable.
+"""
+
+# Printing....
+
+# A
+print("Python is the beast!")
+
+# B
+print("I love coding.")
+
+# C
+print("I am going to master Python soon")
+
+# D
+print("I am learning Python, hooray!!")
+
+# E
+print("Python is the most powerful programming language!")
+
+# -----------------------------------------------------
+
+#  Variables 
+
+# A
+fruit = "Apple"
+print(fruit)
+
+# B
+sentence = "Today Friday"
+print(sentence)
+
+# C
+name = "Myname"
+print(name)
+
+# D
+surname = "Mysurname"
+print(surname)
+
+# E
+full_name = "Firstname Lastname"
+print(full_name)
+
+# F
+color = "Black"
+print(color)
+
+# G
+gender = 'Male'
+print(gender)
+
+# H
+brand = "BMW"
+print(brand)
+
+# -----------------------------------------------
+
---
+++ 1_28.08.2024/homework_09#2.py
@@ -0,0 +1,183 @@
+"""
+- Variables -
+A. Create a variable which's name consists of 2 words. Assign appropriate String value to it.
+B. Create a variable which's name consists of 3 words. Assign appropriate String value to it.
+C. What's the name of the standard variable naming principle in Python?
+D. What's the label (name) of any imaginary created box in computer memory?
+
+- Strings -
+Create variables with appropriate names according to the following topics:
+A. Genre of music
+B. Color
+C. Car brand
+D. Digit
+E. Hobby
+F. Language
+G. Firstname
+H. Lastname
+I. Address
+
+- Comments -
+A. One-line comment in any random three places of your code, if it's appropriate.
+B. Multi-line comment anywhere in your code, if it's appropriate.
+
+Quiz:
+A. A line of text that Python won't try to run as code:
+    a) comment  
+    b) module
+    c) variable
+    d) boolean
+
+B. Which is used to make single line comments?
+    a) %
+    b) #  
+    c) /
+    d) """ """
+    e) \
+
+C. Which Python command (function) is used to output information
+to the screen (terminal, console)?
+    a) read
+    b) output
+    c) print
+    d) display
+
+D. What is the output of the print() function call?
+--------------------------
+|    a = 'foo'           |
+|    b = 'bar'           |
+|    print(a * 2 + b)    |
+--------------------------
+    a) foobar
+    b) barfoo
+    c) barbarfoo
+    d) farboofar
+    e) foofoobar
+
+E. What is the output of the print() function call?
+--------------------------------------
+|    salary = "17000"                |
+|    phrase = "My salary is"         |
+|    print(phrase, salary)           |
+--------------------------------------
+    a) it's not pythonic code
+    b) My salary is17000.
+    c) My salary is 17000
+    d) My salary is17000
+    e) My salary is 17000.
+    f) error
+
+F. What is the output of the print() function call?
+--------------------------------------
+|    salary = "17000"                |
+|    phrase = "My salary is"         |
+|    print(salary, pnrase)           |
+--------------------------------------
+    a) it's not pythonic code
+    b) My salary is17000.
+    c) My salary is 17000
+    d) My salary is17000
+    e) My salary is 17000.
+    f) error
+"""
+
+
+# Variables
+
+# A
+old_city = "Baku"
+print(old_city)
+
+# B
+new_mobile_number = "+99455xxxxxxxx"
+print(new_mobile_number)
+
+# C
+"""
+A-Z, a-z, 0-9, _
+name
+full_name
+name2
+
+"""
+
+# D
+
+"""
+Variable
+"""
+# ----------------------------------------------------
+
+# Strings
+
+genre_of_music = "Mugam"
+color = "Green"
+car_brand = "Audi"
+digit = 777
+hobby = "Reading"
+language = "English"
+firstname = "Ali"
+lastname = "Aliyev"
+address = "Fatali Xan Xoyski"
+
+print(firstname + "" + lastname)
+print(digit, hobby)
+print(f"address is {address}")
+print(f"His car is {car_brand} and its color {color}")
+
+# ----------------------------------------------------
+
+# Comments
+
+# First Line
+# Second line
+# Third Line
+
+"""
+Multi
+Line
+Comment
+
+"""
+
+# -------------------------------------------------------
+
+# Quiz
+
+"""
+A. A line of text that Python won't try to run as code:
+    a) comment  
+
+B. Which is used to make single line comments?
+    b) #  
+
+C. Which Python command (function) is used to output information
+to the screen (terminal, console)?
+    c) print
+
+D. What is the output of the print() function call?
+--------------------------
+|    a = 'foo'           |
+|    b = 'bar'           |
+|    print(a * 2 + b)    |
+--------------------------
+    e) foofoobar
+
+E. What is the output of the print() function call?
+--------------------------------------
+|    salary = "17000"                |
+|    phrase = "My salary is"         |
+|    print(phrase, salary)           |
+--------------------------------------
+    c) My salary is 17000
+
+
+F. What is the output of the print() function call?
+--------------------------------------
+|    salary = "17000"                |
+|    phrase = "My salary is"         |
+|    print(salary, pnrase)           |
+--------------------------------------
+    f) error
+"""
+
