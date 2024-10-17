c941fcde87c2eb13959bf3e22388a78c035a020c
savalanqorxmaz <savalan.qorxmaz@gmail.com>
2024-10-17 20:55:32 +0400

    Lesson 22

---
+++ 22_17.10.2024/task.py
@@ -0,0 +1,6 @@
+'''
+Kitabxana Kataloq Sistemi :  İstifadəçilərin öz kitabları ilə kitab əlavə edə biləcəyi kitabxana 
+kataloqu sistemi yaradınbaşlıqlar
+ və müəlliflər, kitabları silin, kitabları başlıq və ya müəllifə görə axtarın və siyahısını göstərin
+mövcud kitablar. Kitab adlarını və müəllifləri saxlamaq üçün lüğətdən istifadə edin və for looplarından,
+ while, function,  istifadə edin kataloq əməliyyatlarının həyata keçirilməsi.'''
